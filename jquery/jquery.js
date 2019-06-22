751952b5bb8e98eab09ab3c0933c156fff7f5f89
denimoclock69 <50457490+denimoclock69@users.noreply.github.com>
2019-06-22 07:52:12 -0400

    Add files via upload

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,1 +1,20 @@
+window.onload = function() {
+ 
+    alert( "Hello Visitor" );
+    $( "a" ).click(function( event ) {
+        alert( "Thanks for visiting!" );
+ 
+    });
 
+};
+
+$(document).ready(function() {
+	$("#button").click(
+		function() {
+			$("body").css("color", "#5a82b4");
+		}
+	);
+
+	$('.gif').gifplayer();
+	$('#multiscroll').multiscroll();
+});
